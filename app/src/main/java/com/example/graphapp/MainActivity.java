fcb096c01f2f66d1978cc376c04c4e47f33e3a3b
Max Gogats <mgogats@gmail.com>
2019-09-16 14:55:46 -0400

    working with mpandroidChart

--- app/src/main/java/com/example/graphapp/MainActivity.java
+++ app/src/main/java/com/example/graphapp/MainActivity.java
@@ -12,53 +12,58 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+public class MainActivity extends AppCompatActivity  {
 
-    private LinearLayout customViewLayout = null;
-    private Button startGame = null;
-    MySurface mySurface = null;
-    public boolean drawGraph = true;
+    BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initControls();
+        barChart = (BarChart) findViewById(R.id.barGraph);
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
 
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(.5f, 80f));
+        entries.add(new BarEntry(1.5f, 60f));
+        entries.add(new BarEntry(2.5f, 50f));
+        // gap of 2f
+        entries.add(new BarEntry(4.5f, 70f));
+        entries.add(new BarEntry(5.5f, 60f));
 
-        // Create custom surfaceview object.
-        MySurface mySurface = new MySurface(getApplicationContext());
-
-        mySurface.setOnTouchListener(this);
-
-        customViewLayout.addView(mySurface);
-
-        startGame.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                drawGraph = false;
-            }
-        });
+        List<String> dates = new ArrayList<>();
+        dates.add("May1");
+        dates.add("May2");
+        dates.add("May3");
+        dates.add("May4");
+        dates.add("May5");
 
 
-    }
+        BarDataSet set = new BarDataSet(entries, "BarDataSet");
+        BarData theData = new BarData(set);
 
-    public void initControls() {
-        if (customViewLayout == null) {
-            customViewLayout = (LinearLayout) findViewById(R.id.customViewLayout);
-        }
+        set.setColor(Color.BLUE);
 
-        if (startGame == null) {
-            startGame = findViewById(R.id.startGame);
-        }
-    }
+        theData.setBarWidth(0.9f); // set custom bar width
+        barChart.setData(theData);
+        barChart.setFitBars(true); // make the x-axis fit exactly all bars
+        barChart.invalidate(); // refresh
 
-    /* If user finger touch the surfaceview object. */
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
+        barChart.setScaleEnabled(true);
+        barChart.setTouchEnabled(true);
+        barChart.setDragEnabled(true);
 
     }
 }
